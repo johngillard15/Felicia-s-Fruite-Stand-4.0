c6af2d18c9fdc09271a2e25071914fd4a2bd9d75
johngillard15 <johngillard15@gmail.com>
2021-08-26 18:40:34 -0400

    added ouput to double method

--- src/com/utilities/Input.java
+++ src/com/utilities/Input.java
@@ -54,6 +54,13 @@ public class Input {
 
             validChoice =
                     InputValidator.validDouble(input) && (Double.parseDouble(input) >= MIN && Double.parseDouble(input) <= MAX);
+
+            if(!InputValidator.validInt(input))
+                System.out.println("That is not a valid number value. Please try again");
+            else if(MIN != Double.MIN_VALUE && !(Double.parseDouble(input) >= MIN))
+                System.out.println("Value must be greater than or equal to " + MIN);
+            else if(MAX != Double.MAX_VALUE && !(Double.parseDouble(input) <= MAX))
+                System.out.println("Value must be less than or equal to " + MAX);
         }while(!validChoice);
 
         return Double.parseDouble(input);
