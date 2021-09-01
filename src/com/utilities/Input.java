6e8c42021247e97e5c2a38bb0a07f2e5ace032f2
johngillard15 <johngillard15@gmail.com>
2021-09-01 13:26:57 -0400

    added updated Input class, removed InputValidator

--- src/com/utilities/Input.java
+++ src/com/utilities/Input.java
@@ -1,13 +1,49 @@
 package com.utilities;
 
+import java.util.Arrays;
 import java.util.Scanner;
+
+/**
+ * <p>Contains methods that manage grabbing user input and handling invalid data. </p>
+ *
+ * <p>This class contains methods that handle runtime exceptions involving user input, mainly involving type conversions
+ * from String to a number format (Integer, Double, etc.).</p>
+ *
+ * @author John Gillard
+ * @since 13/8/2021
+ */
 
 public class Input {
     private static final Scanner scan = new Scanner(System.in);
 
-    public static String getString(){
+    public static String getInput(){
         System.out.print("-> ");
-        return scan.nextLine();
+        return scan.nextLine().trim();
+    }
+
+    public static String getString(String... validChoices){
+        return getString(false, validChoices);
+    }
+    public static String getString(boolean isCaseSensitive, String... validChoices){
+        String input;
+
+        boolean validInput;
+        do{
+            input = getInput();
+
+            validInput = validChoices.length == 0 || checkStringOptions(input, isCaseSensitive, validChoices);
+
+            if(!validInput)
+                System.out.printf("\"%s\" is not a valid option. Please try again.", input);
+        }while(!validInput);
+
+        return input;
+    }
+
+    public static boolean checkStringOptions(String input, boolean isCaseSensitive, String... validChoices){
+        if(isCaseSensitive)
+            return Arrays.asList(validChoices).contains(input);
+        return Arrays.stream(validChoices).anyMatch(input::equalsIgnoreCase);
     }
 
     public static int getInt(){
@@ -19,21 +55,20 @@ public class Input {
     public static int getInt(final int MIN, final int MAX){
         String input;
 
-        boolean validChoice;
+        boolean validInput;
         do{
-            System.out.print("-> ");
-            input = scan.nextLine();
+            input = getInput();
 
-            validChoice =
-                    InputValidator.validInt(input) && (Integer.parseInt(input) >= MIN && Integer.parseInt(input) <= MAX);
+            validInput =
+                    checkInt(input) && (Integer.parseInt(input) >= MIN && Integer.parseInt(input) <= MAX);
 
-            if(!InputValidator.validInt(input))
-                System.out.println("That is not a valid number value. Please try again");
+            if(!checkInt(input))
+                System.out.printf("\"%s\" is not a valid number value. Please try again\n", input);
             else if(MIN != Integer.MIN_VALUE && !(Integer.parseInt(input) >= MIN))
                 System.out.println("Value must be greater than or equal to " + MIN);
             else if(MAX != Integer.MAX_VALUE && !(Integer.parseInt(input) <= MAX))
                 System.out.println("Value must be less than or equal to " + MAX);
-        }while(!validChoice);
+        }while(!validInput);
 
         return Integer.parseInt(input);
     }
@@ -44,25 +79,53 @@ public class Input {
     public static double getDouble(final double MIN){
         return getDouble(MIN, Double.MAX_VALUE);
     }
-    public static Double getDouble(final double MIN, final double MAX){
+    public static double getDouble(final double MIN, final double MAX){
         String input;
 
-        boolean validChoice;
+        boolean validInput;
         do{
-            System.out.print("-> ");
-            input = scan.nextLine();
+            input = getInput();
 
-            validChoice =
-                    InputValidator.validDouble(input) && (Double.parseDouble(input) >= MIN && Double.parseDouble(input) <= MAX);
+            validInput =
+                    checkDouble(input) && (Double.parseDouble(input) >= MIN && Double.parseDouble(input) <= MAX);
 
-            if(!InputValidator.validDouble(input))
+            if(!checkDouble(input))
                 System.out.println("That is not a valid number value. Please try again");
             else if(MIN != Double.MIN_VALUE && !(Double.parseDouble(input) >= MIN))
                 System.out.println("Value must be greater than or equal to " + MIN);
             else if(MAX != Double.MAX_VALUE && !(Double.parseDouble(input) <= MAX))
                 System.out.println("Value must be less than or equal to " + MAX);
-        }while(!validChoice);
+        }while(!validInput);
 
         return Double.parseDouble(input);
+    }
+
+    /**
+     * <p>Attempts to convert the value of the passed parameter to an Integer value.</p>
+     *
+     * @param checkMe the String to convert
+     * @return a boolean, which determines whether the value can be converted to an Integer
+     *
+     * @throws NumberFormatException if the specified String value cannot be converted to an Integer (not really
+     * anymore but it did)
+     */
+    public static boolean checkInt(String checkMe){
+        try{
+            Integer.parseInt(checkMe);
+            return true;
+        }
+        catch(NumberFormatException e){
+            return false;
+        }
+    }
+
+    public static boolean checkDouble(String checkMe){
+        try{
+            Double.parseDouble(checkMe);
+            return true;
+        }
+        catch(NumberFormatException e){
+            return false;
+        }
     }
 }
