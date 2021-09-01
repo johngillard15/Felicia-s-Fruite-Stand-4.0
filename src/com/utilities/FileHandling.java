45c12507bfd417cce50aae0d1454017928e47ede
johngillard15 <johngillard15@gmail.com>
2021-08-31 20:08:13 -0400

    added FileHandling to store objects in a file

---
+++ src/com/utilities/FileHandling.java
@@ -0,0 +1,36 @@
+package com.utilities;
+
+import java.io.*;
+
+public class FileHandling {
+
+    public static void write(Object obj){
+        try{
+            FileOutputStream writeData = new FileOutputStream("object.txt");
+            ObjectOutputStream writeStream = new ObjectOutputStream(writeData);
+
+            writeStream.writeObject(obj);
+            writeStream.flush();
+            writeStream.close();
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Object read(){
+        Object obj = null;
+
+        try{
+            FileInputStream readData = new FileInputStream("object.txt");
+            ObjectInputStream readStream = new ObjectInputStream(readData);
+
+            obj = readStream.readObject();
+            readStream.close();
+        }catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return obj;
+    }
+}
