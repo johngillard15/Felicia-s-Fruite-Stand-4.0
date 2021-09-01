dc18bde985f80dd4f09e37e03178b5d5102c3c9b
johngillard15 <johngillard15@gmail.com>
2021-09-01 17:28:20 -0400

    added indentation parameter to listerator

--- src/com/utilities/UI.java
+++ src/com/utilities/UI.java
@@ -3,20 +3,32 @@ package com.utilities;
 public class UI { // TODO: make different listing methods (bullet points, letters, numerals, etc.)
 
     public static void listerator(String... listElements){
-        listerator(1, listElements);
+        listerator(1, 0, listElements);
     }
-    public static void listerator(int listType, String... listElements){
+
+    /**
+     * <p>1 = number, 2, letter</p>
+     * <p>subLevel refers to the level of indentation before each list element</p>
+     * @param listType
+     * @param subLevel
+     * @param listElements
+     */
+    public static void listerator(int listType, int subLevel, String... listElements){
+        String subLevelIndent = "";
+        for(int i = 0; i < subLevel; i++)
+            subLevelIndent += "\t";
+
         switch(listType){
             case 1 -> { // number
                 int listNum = 0;
                 for(String option : listElements){
-                    System.out.printf("%d. %s\n", ++listNum, option);
+                    System.out.printf("%s%d. %s\n", subLevelIndent, ++listNum, option);
                 }
             }
             case 2 -> { // letter
                 char c = 'a';
                 for(String option : listElements){
-                    System.out.printf("%c. %s\n", ++c, option);
+                    System.out.printf("%s%c. %s\n", subLevelIndent, ++c, option);
                 }
             }
 
