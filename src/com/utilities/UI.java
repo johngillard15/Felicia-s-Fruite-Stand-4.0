3360b889ab1ce2e05798771d30171f17d1869238
johngillard15 <johngillard15@gmail.com>
2021-09-02 18:40:28 -0400

    added updated UI class

--- src/com/utilities/UI.java
+++ src/com/utilities/UI.java
@@ -1,15 +1,28 @@
 package com.utilities;
 
-public class UI { // TODO: make different listing methods (bullet points, letters, numerals, etc.)
+public class UI {
 
+    /**
+     * <p>Creates a list out of the provided String arguments. Prints a numbered list by default.</p>
+     *
+     * @param listElements the information to listerate
+     */
     public static void listerator(String... listElements){
         listerator(1, 0, listElements);
     }
-
     /**
-     * <p></p>
+     * <p>Creates a list out of the provided String arguments. Supports multiple list types.</p>
      *
-     * @param listType the kind of list to create (1 - number, 2 - letter)
+     * @param listType the kind of list to create (1 - number, 2 - letter, 3 - numeral, 4 - bullet)
+     * @param listElements the information to listerate
+     */
+    public static void listerator(int listType, String... listElements){
+        listerator(listType, 0, listElements);
+    }
+    /**
+     * <p>Creates a list out of the provided String arguments. Supports multiple list types and indentation levels.</p>
+     *
+     * @param listType the kind of list to create (1 - number, 2 - letter, 3 - numeral, 4 - bullet)
      * @param subLevel level of indentation to apply to the list
      * @param listElements the information to listerate
      */
@@ -26,13 +39,34 @@ public class UI { // TODO: make different listing methods (bullet points, letter
                 }
             }
             case 2 -> { // letter
-                char c = 'a';
+                for(int i = 0; i < listElements.length; i++){
+                    System.out.printf("%s%s. %s\n", subLevelIndent, getLetterValue(i), listElements[i]);
+                }
+            }
+            case 3 -> { // numeral
+
+            }
+            case 4 -> { // bullet
                 for(String option : listElements){
-                    System.out.printf("%s%c. %s\n", subLevelIndent, ++c, option);
+                    System.out.printf("%s• %s\n", subLevelIndent, option);
                 }
             }
 
             default -> throw new IllegalStateException("Invalid list type: " + listType);
         }
+    }
+
+    /**
+     * Converts a number to a String of letters.
+     *
+     * @param listNum the number to convert
+     * @return a String of letters representing the passed number value
+     */
+    public static String getLetterValue(int listNum){
+        int nextLetter = listNum / 26;
+        int letterNum = listNum % 26;
+        char letter = (char)((int)'A' + letterNum);
+
+        return (nextLetter == 0 ? "" : getLetterValue(nextLetter - 1)) + letter;
     }
 }
